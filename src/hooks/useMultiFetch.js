f6e2fef1dbe5b9afd5aecc6cb851c8c68ac0d265
jinxyang <jinx@jinxyang.com>
2022-05-19 17:35:18 +0800

    fix(useMultiFetch): loading & loaded

--- src/hooks/useMultiFetch.js
+++ src/hooks/useMultiFetch.js
@@ -15,34 +15,30 @@ const initialState = {
 const useMultiFetch = (service = () => {}, callbackOrDelay, delay = 0) => {
   const prevId = React.useRef(defaultId)
   const id = React.useRef(defaultId)
+  const loaded = React.useRef(false)
 
   const [state, setState] = React.useState({
-    [defaultId]: {
-      loading: false,
-      loaded: false,
-      list: [],
-    },
+    [defaultId]: { ...initialState },
   })
+
   const fetch = useFetch(service, { autoAbort: false })[1]
 
   const start = React.useCallback(
     async (payloadList) => {
       const newId = nanoid()
-
       id.current = newId
 
       setState((state) => ({
         ...state,
-        [newId]: { loading: true, ...initialState },
+        [newId]: { loading: true, loaded: loaded.current, list: [] },
       }))
-
       const list = await Promise.all(_.map(payloadList, fetch))
-
       setState((state) => ({
         ...state,
         [newId]: { loading: false, loaded: true, list },
       }))
       prevId.current = newId
+      loaded.current = true
     },
     [fetch],
   )
